8075e1d4fa48bf895b6286a3621257346d27bc22
张晓旸 <zhangxiaoyang@news.cn>
2016-10-13 14:34:43 +0800

    add jasonUtils

---
+++ src/main/java/com/eudemon/taurus/app/utils/JasonUtils.java
@@ -0,0 +1,30 @@
+package com.eudemon.taurus.app.utils;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSON;
+
+public class JasonUtils {
+	private static Logger logger = LoggerFactory.getLogger(JasonUtils.class);
+	
+	public static void writeJasonP(HttpServletRequest request, HttpServletResponse response, Object data){
+		try {
+			String callback = request.getParameter("callback");
+			String jsn = JSON.toJSONString(data);
+			String jp = jsn;
+			if (null != callback && !callback.equals("")) {
+				jp = callback + "(" + jsn + ")";
+			}
+			response.setCharacterEncoding("utf-8");
+			response.getWriter().println(jp);
+		} catch (IOException e) {
+			logger.error("write jasonp exception, data : " + data, e);
+		}
+	}
+}
